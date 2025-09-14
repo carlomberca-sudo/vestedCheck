d64e9299345dffb3911d43c410d2259bb2fd24f9
carlomberca-sudo <carlomberca@gmail.com>
2025-09-15 01:23:41 +0200

    Update vesteda_watch.py

--- vesteda_watch.py
+++ vesteda_watch.py
@@ -42,8 +42,8 @@ VESTEDA_URL = (
 CHECK_INTERVAL_MINUTES = 5
 
 # Working window (local Amsterdam time)
-WORKDAY_START = dtime(0, 0)   # 09:00
-WORKDAY_END = dtime(23, 59)    # 17:00 (inclusive)
+WORKDAY_START = dtime(9, 0)   # 09:00
+WORKDAY_END = dtime(17, 0)    # 17:00 (inclusive)
 
 # Notify only when count INCREASES (True) or on ANY change (False)
 ONLY_NOTIFY_ON_INCREASE = False
